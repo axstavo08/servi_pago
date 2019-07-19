bff3b6f968cb37760193ceea2489c8dce65619c2
Gustavo Ramos <tavo15@gmail.com>
2019-07-19 14:43:34 -0500

    response

--- backend/banco/src/main/java/com/example/demo/controller/ClienteController.java
+++ backend/banco/src/main/java/com/example/demo/controller/ClienteController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ClienteDto;
+import com.example.demo.dto.ResponseBody;
+import com.example.demo.dto.ResponseData;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,36 +12,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Categoria;
-import com.example.demo.model.Cliente;
 import com.example.demo.model.Pago;
 import com.example.demo.model.Suscripcion;
 import com.example.demo.model.Tarjeta;
 import com.example.demo.service.ClienteService;
 import com.example.demo.service.ConsultaService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
 
-	@Autowired
-	private ClienteService clienteservice;
-	
-	@GetMapping("/suscripciones")
-	public List<Suscripcion> ListarSuscripciones(String dni) {
-		List<Suscripcion> suscripciones = clienteservice.getSuscripciones(dni);		
-		return suscripciones;
-	}
-	
-	@GetMapping("/pagos")
-	public List<Pago> ListarPagos(String dni) {
-		List<Pago> pagos = clienteservice.getPagos(dni);
-		return pagos;
-	}
-	
-	@GetMapping("/tarjetas")
-	public List<Tarjeta> ListarTarjetas(String dni) {
-		List<Tarjeta> tarjetas = clienteservice.getTarjetas(dni);
-		return tarjetas;
-	}
+    @Autowired
+    private ClienteService clienteservice;
+
+    @PostMapping("login")
+    public Object obtenerCliente(String dni, String password) {
+        ClienteDto cliente = clienteservice.login(dni, password);
+        if (cliente == null) {
+            return new ResponseBody(1, new ResponseData("Usuario incorrecto o invalido"));
+        }
+        return new ResponseBody(1, new ResponseData("Usuario incorrecto o invalido"));
+    }
+
+    @GetMapping("/suscripciones")
+    public List<Suscripcion> ListarSuscripciones(String dni) {
+        List<Suscripcion> suscripciones = clienteservice.getSuscripciones(dni);
+        return suscripciones;
+    }
+
+    @GetMapping("/pagos")
+    public List<Pago> ListarPagos(String dni) {
+        List<Pago> pagos = clienteservice.getPagos(dni);
+        return pagos;
+    }
+
+    @GetMapping("/tarjetas")
+    public List<Tarjeta> ListarTarjetas(String dni) {
+        List<Tarjeta> tarjetas = clienteservice.getTarjetas(dni);
+        return tarjetas;
+    }
 }
--- backend/banco/src/main/java/com/example/demo/service/ClienteService.java
+++ backend/banco/src/main/java/com/example/demo/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.ClienteDto;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,44 +16,48 @@ import com.example.demo.repository.IClienteRepo;
 @Service
 public class ClienteService {
 
-	@Autowired
-	private IClienteRepo clienterepo;
-	
-	public List<Cliente> getClientes(){
-		return clienterepo.findAll();
-	}
-	
-	public List<Suscripcion> getSuscripciones(String dni){
-		Cliente cliente = clienterepo.getOne(dni);
-		return cliente.getSuscripciones();
-	}
-	
-	public List<Pago> getPagos(String dni){
-		Cliente cliente = clienterepo.getOne(dni);
-		List<Pago> pagos = new ArrayList<Pago>();
-		List<Tarjeta> tarjetas = cliente.getTarjetas();
-		for(Tarjeta t : tarjetas) {
-			pagos.addAll(t.getPagos());
-		}
-		return pagos;
-	}
-	
-	public List<Tarjeta> getTarjetas(String dni){
-		Cliente cliente = clienterepo.getOne(dni);		
-		return cliente.getTarjetas();
-	}
-	
-	public boolean addCliente(Cliente cliente) {
-		Cliente c = clienterepo.save(cliente);
-		return c != null;
-	}
-	
-	public Cliente login(String dni,String password) {
-		Cliente c = clienterepo.getOne(dni);
-		if(c.getPassword() == password) {
-			return c;
-		}
-		return null;
-	}
-	
+    @Autowired
+    private IClienteRepo clienterepo;
+
+    public List<Cliente> getClientes() {
+        return clienterepo.findAll();
+    }
+
+    public List<Suscripcion> getSuscripciones(String dni) {
+        Cliente cliente = clienterepo.getOne(dni);
+        return cliente.getSuscripciones();
+    }
+
+    public List<Pago> getPagos(String dni) {
+        Cliente cliente = clienterepo.getOne(dni);
+        List<Pago> pagos = new ArrayList<Pago>();
+        List<Tarjeta> tarjetas = cliente.getTarjetas();
+        for (Tarjeta t : tarjetas) {
+            pagos.addAll(t.getPagos());
+        }
+        return pagos;
+    }
+
+    public List<Tarjeta> getTarjetas(String dni) {
+        Cliente cliente = clienterepo.getOne(dni);
+        return cliente.getTarjetas();
+    }
+
+    public boolean addCliente(Cliente cliente) {
+        Cliente c = clienterepo.save(cliente);
+        return c != null;
+    }
+
+    public ClienteDto login(String dni, String password) {
+        Cliente cliente = clienterepo.findByDni(dni);
+        if (cliente == null) {
+            return null;
+        }
+        if (!cliente.getPassword().equals(password)) {
+            return null;
+        }
+        return new ClienteDto(cliente.getDni(), cliente.getNombres(), cliente.getApellidos(),
+                cliente.getFecha_nacimiento(), cliente.getTelefono(), cliente.getCorreo());
+    }
+
 }
